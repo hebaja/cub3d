f9b7f21e993d0e121309a0200c59d4a82dc28485
Henrique <hebaja@yahoo.com.br>
2025-07-31 16:23:02 -0300

    fix: 🐛 Static Ray Cast
    Fixing validation of map, avoiding leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dbatista <dbatista@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 19:38:30 by hebatist          #+#    #+#             */
-/*   Updated: 2025/07/31 16:50:00 by hebatist         ###   ########.fr       */
+/*   Updated: 2025/07/31 19:21:19 by hebatist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,15 @@ int	main(int argc, char **argv)
 		st_file = build_st_file(argv[1]);
 		if (!st_file)
 			return (1);
-		parse_elements(st_file);
+		if (!parse_elements(st_file))
+		{
+			clean_st_file(st_file);
+			return (1);
+		}
+		st_coord = build_st_coord(st_file);
+		st_mlx = build_st_mlx(st_file, st_coord, 896, 640);
+		ray_cast(st_mlx);
+		clean_all(st_mlx);
 	}
-	st_coord = build_st_coord(st_file);
-	st_mlx = build_st_mlx(st_file, st_coord, 896, 640);
-	ray_cast(st_mlx);
-	clean_all(st_mlx);
 	return (0);
 }
