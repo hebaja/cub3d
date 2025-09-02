f98cdb62fee3cf360473da9d1602221308aa4114
hebatist <hebaja@yahoo.com.br>
2025-09-01 22:45:35 -0300

    fix: 🐛 Main
    Minimap drawing full fixed

--- src/bonus/minimap1_bonus.c
+++ src/bonus/minimap1_bonus.c
@@ -45,7 +45,6 @@ int	init_minimap(t_mlx *st_mlx)
 {
 	int	mm_size;
 
-	mm_size = (int)(st_mlx->screen_height * 0.28) - 1;
 	st_mlx->minimap = (t_img *)malloc(sizeof(t_img));
 	if (st_mlx->minimap == NULL)
 		return (0);
@@ -56,14 +55,16 @@ int	init_minimap(t_mlx *st_mlx)
 	st_mlx->minimap_anim_dir = 0;
 	st_mlx->minimap_frame = 0;
 	st_mlx->minimap_time = 0;
+	st_mlx->minimap_size = 7;
+	mm_size = (int)(st_mlx->screen_height * 0.28);
+	mm_size = (mm_size / st_mlx->minimap_size) * st_mlx->minimap_size;
 	st_mlx->minimap->width = mm_size;
 	st_mlx->minimap->height = mm_size;
+	st_mlx->minimap_block_size = st_mlx->minimap->width / st_mlx->minimap_size;
 	st_mlx->minimap->img = mlx_new_image(st_mlx->mlx, mm_size, mm_size);
 	st_mlx->minimap->img_addr = mlx_get_data_addr(st_mlx->minimap->img, \
 		&st_mlx->minimap->bpp, \
 		&st_mlx->minimap->size_line, \
 		&st_mlx->minimap->endian);
-	st_mlx->minimap_size = 7;
-	st_mlx->minimap_block_size = st_mlx->minimap->width / st_mlx->minimap_size;
 	return (1);
 }
